b87bb25703dc0274ceb8153021490dec47dc0a43
kinfuhaile2 <kinfuhaile2@gmail.com>
2019-01-26 10:33:07 -0500

    Wire Farme done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fintness-tracker';
+  shouldRun = true;
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule, MatIconModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule} from '@angular/material';
+
 
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-    exports:[MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule,
+         MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule],
+    exports:[MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, 
+        MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatListModule]
 })
 export class MaterialModule{}
